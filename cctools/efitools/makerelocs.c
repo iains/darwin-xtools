f1746db1a20b730d3487d9c0d98f2ec6658dbafb
Iain Sandoe <iain@codesourcery.com>
2015-05-16 16:54:30 +0100

    efitools - Add --version commands to the tools.

--- cctools/efitools/makerelocs.c
+++ cctools/efitools/makerelocs.c
@@ -116,6 +116,13 @@ char **envp)
 	output = NULL;
 
 	for(i = 1; i < argc; i++){
+	    if(strcmp(argv[i], "--version") == 0){
+		/* Implement a gnu-style --version.  */
+		char *pnam = strrchr(progname, '/');
+		pnam = (pnam)?pnam+1:progname;
+		fprintf(stderr, "xtools %s - based on Apple Inc. %s\n", pnam, apple_version);
+		exit(0);
+	    }
 	    if(strcmp(argv[i], "-v") == 0)
 		verbose = TRUE;
 	    else if(input == NULL)
