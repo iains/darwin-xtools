1efba18284bb2e0bd536f031b0f1bd10170d2d6f
Fabian Groffen <grobian@gentoo.org>
2020-12-19 20:31:17 +0100

    [cctools] fix compilation then LTO_SUPPORTED is NO
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- cctools/misc/strings.c
+++ cctools/misc/strings.c
@@ -354,7 +354,11 @@ void *cookie)
 	 * If the ofile is not an object file then process it without reguard
 	 * to sections.
 	 */
-	if(ofile->object_addr == NULL || ofile->member_type == OFILE_LLVM_BITCODE){
+	if(ofile->object_addr == NULL
+#ifdef LTO_SUPPORT
+			|| ofile->member_type == OFILE_LLVM_BITCODE
+#endif
+			){
 	    if(ofile->file_type == OFILE_FAT && ofile->arch_flag.cputype != 0){
 		if(ofile->fat_header->magic == FAT_MAGIC_64){
 		    addr = ofile->file_addr +
