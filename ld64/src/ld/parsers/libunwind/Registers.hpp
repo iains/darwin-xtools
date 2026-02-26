4c77f0f009c3bc9038dbdd1ce35c9be1825ce6c1
Eric Gallager <egall@gwmail.gwu.edu>
2026-02-26 13:36:35 -0500

    Apply patches from MacPorts
    see:
    https://github.com/macports/macports-ports/tree/master/devel/darwin-xtools/files

--- ld64/src/ld/parsers/libunwind/Registers.hpp
+++ ld64/src/ld/parsers/libunwind/Registers.hpp
@@ -43,6 +43,55 @@
 namespace libunwind {
 
 
+#ifndef i386_thread_state_t
+struct _i386_thread_state_t
+{
+    unsigned int	__eax;
+    unsigned int	__ebx;
+    unsigned int	__ecx;
+    unsigned int	__edx;
+    unsigned int	__edi;
+    unsigned int	__esi;
+    unsigned int	__ebp;
+    unsigned int	__esp;
+    unsigned int	__ss;
+    unsigned int	__eflags;
+    unsigned int	__eip;
+    unsigned int	__cs;
+    unsigned int	__ds;
+    unsigned int	__es;
+    unsigned int	__fs;
+    unsigned int	__gs;
+};
+#define i386_thread_state_t _i386_thread_state_t
+#endif
+#ifndef x86_thread_state64_t
+struct _x86_thread_state64_t
+{
+	__uint64_t	__rax;
+	__uint64_t	__rbx;
+	__uint64_t	__rcx;
+	__uint64_t	__rdx;
+	__uint64_t	__rdi;
+	__uint64_t	__rsi;
+	__uint64_t	__rbp;
+	__uint64_t	__rsp;
+	__uint64_t	__r8;
+	__uint64_t	__r9;
+	__uint64_t	__r10;
+	__uint64_t	__r11;
+	__uint64_t	__r12;
+	__uint64_t	__r13;
+	__uint64_t	__r14;
+	__uint64_t	__r15;
+	__uint64_t	__rip;
+	__uint64_t	__rflags;
+	__uint64_t	__cs;
+	__uint64_t	__fs;
+	__uint64_t	__gs;
+};
+#define x86_thread_state64_t _x86_thread_state64_t
+#endif
 ///
 /// Registers_x86 holds the register state of a thread in a 32-bit intel process.  
 ///
---
+++ tapilite/include/tapi/tapi.h
@@ -0,0 +1,34 @@
+//===-- tapi/tapi.h - TAPI C++ Library Interface ----------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief This is the umbrella header for the TAPI C++ Library Interface.
+/// \since 1.0
+///
+//===----------------------------------------------------------------------===//
+//
+// Modified version for "tapilite", a standalone just enough
+// implementation for use without LLVM
+//
+#ifndef TAPI_H
+#define TAPI_H
+
+///
+/// \defgroup TAPI_CPP_API TAPI C++ API
+///
+/// The C++ Application Programming Interface (API) for the TAPI library
+///
+
+#include <tapi/Defines.h>
+#include <tapi/LinkerInterfaceFile.h>
+#include <tapi/PackedVersion32.h>
+#include <tapi/Symbol.h>
+#include <tapi/Version.h>
+
+#endif // TAPI_H
